60cdc2e56f50e920b6421e62404ab7c2075af6db
Thomas <s00119500@mail.itsligo.ie>
2014-03-22 22:34:30 +0000

    first commit
    added model class trips,legs and guest
    added a data access layer folder with a context class
    enabled migrations

---
+++ TravelAwayFarAway/TravelAwayFarAway/DAL/ContextClass.cs
@@ -0,0 +1,16 @@
+﻿using MvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TravelAwayFarAway.DAL
+{
+    public class ContextClass : DbContext
+    {
+        public DbSet<Trip> trip { get; set; }
+        public DbSet<Leg> leg { get; set; }
+        public DbSet<Guest> guest { get; set; }  
+    }
+}
---
+++ TravelAwayFarAway/TravelAwayFarAway/Models/All.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Models
+{
+    
+    public class Trip
+    {
+
+        //[Key]
+        //[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        
+        public int TripId { get; set; }
+        public string name { get; set; }  // i.e sahara adventure
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int minimunNumberOfGuests { get; set; } // min amount required to be valid
+
+        public virtual List<Leg> Leg { get; set; }
+        //public List<Guest> totalAssociatedGuests { get; set; }
+    }
+
+    public class Leg//: Trip
+    {
+        //[Key]
+        //[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    
+        public int LegId { get; set; }
+        //public int tripId { get; set; }
+        public string legName { get; set; }
+        public string startLocation { get; set; } // city name 
+        public string endLocation { get; set; }   // city name 
+        public DateTime LegtartDate { get; set; }
+        public DateTime legEndDate { get; set; }
+
+        public virtual List<Guest> LegAssociatedGuests{ get; set; }
+    }
+
+    public class Guest
+    {
+        //[Key]
+        //[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+      
+
+        public int GuestId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Leg> listOfLeg { get; set; }
+    }
+}
